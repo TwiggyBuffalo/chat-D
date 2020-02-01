da0f7eb1ac7fe2f9aa1528d4661088d06c3ab112
TwiggyBuffalo <danielcurran@live.co.za>
2020-02-01 20:48:57 +0200

    pushing all my sensitive info such as api key etc

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+  firebase: {
+    apiKey: "AIzaSyAM8oaCvKusDesJxNt-HZVGKQ0JzP5nOhg",
+    authDomain: "chat-d-a3118.firebaseapp.com",
+    databaseURL: "https://chat-d-a3118.firebaseio.com",
+    projectId: "chat-d-a3118",
+    storageBucket: "chat-d-a3118.appspot.com",
+    messagingSenderId: "258327410310",
+    appId: "1:258327410310:web:164e8bd8dfe766837a4307",
+    measurementId: "G-1MDD8MPYTM"
+  }
+}
